df014955bf51024b93d16c2ceb1a09a3bbc0af86
Swimmy-Shakujiikoen <164879661+Swimmy-Shakujiikoen@users.noreply.github.com>
2024-03-31 11:15:49 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-# 使用するモジュールのインポート
-import btncon
+"""スマホのボタン側のプログラム
+
+© 2024 Swimmy石神井公園校
+"""
+
 import js
+
+import btncon
 
 # ボタンのコントローラーを発動
 btn = btncon.ButtonController()
 
+# 準備OKでないなら、ボタンを失敗マークにする
+if not btn.is_ready():
+    js.btn_failure()
+
 
 # ボタン発動時
 async def btn_click(event) -> None:
-    await btn.sleep(5)
+    await btn.send_trigger()
+
+    # 信号の送信に失敗した場合はボタンを失敗マークにする
+    if not btn.is_send_ok():
+        js.btn_failure()
+        return
+
+    # IoT側が動作完了するまで10秒ごとに確認する
+    while not await btn.is_done():
+        await btn.sleep(10)
 
     js.btn_success()
